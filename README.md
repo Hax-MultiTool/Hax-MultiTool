e2f79a7755a2f0b009a17ea7b151da495142b816
Chroma <110534939+uziff@users.noreply.github.com>
2025-12-19 18:45:46 +0100

    Uploaded v0.5 files

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hax MultiTool v0.5
+A development file i found on a random Google Drive account
---
+++ hax.bat
@@ -0,0 +1,105 @@
+@echo off
+mode con lines=28 cols=64
+:fail
+cls
+color 9
+title Hax
+echo.
+echo.
+echo LOGIN PANNEL
+echo.
+set /p user= Username - 
+echo.
+set /p pass= Password - 
+echo.
+echo [40;30m                      [40;35mWaking Up TOOL . . .
+ping localhost -n 2 >nul
+if %pass% == WakeUp goto main
+echo.
+echo [40;30m                      [40;31 Wrong Password!
+timeout 2 >nul
+goto fail
+:main
+echo.
+echo [40;30m                  [40;32m WELLCOME  BACK  %user%!!
+ping localhost -n 2 >nul
+cls
+title ~~ tool Main Menu ~~ made by chroma ~~
+:menu
+echo Welcome  Back  %user%!
+echo .
+echo [40;30m ================================================================
+echo [40;30m # 1. Better Pinger
+echo [40;30m # 2. Office Activator 2021
+echo [40;30m #
+echo [40;30m #
+echo [40;30m #
+echo [40;30m ================================================================
+echo [40;30m .
+echo [40;30m .
+ping localhost -n 2 >nul
+ping localhost -n 2 >nul
+set /p MENU= [40;30m - - - - - - hax menu - 
+if %MENU% == Better Pinger goto PINGER
+if %MENU% == 1 goto PINGER
+:PINGER
+cls
+title better pinger - by Chroma toggled - OFF
+:START
+cls
+color F
+echo =================================================================
+echo .
+echo  Better Pinger
+echo .
+echo =================================================================
+echo .
+echo  By Chroma Toggled 
+echo .
+echo ==================================================================
+echo .
+echo  Copyright: JustEma
+echo .
+echo ==================================================================
+echo .
+echo .
+echo .
+set /p CONTINUE=sicuro di voler continuare? 
+if %CONTINUE% == si goto PINGER
+if %CONTINUE% == Si goto PINGER
+if %CONTINUE% == ok goto PINGER
+if %CONTINUE% == sicuramente goto PINGER
+if %CONTINUE% == certamente goto PINGER
+if %CONTINUE% == continua goto PINGER
+goto START
+:PINGER
+color a
+title better pinger - by Chroma toggled - ON
+set /p IP=inserisci IP-
+set /p BYTE=numero di bytes-
+title better pinger - by Chroma toggled - BUSY
+ping -t %ip% -l %byte% -w 1
+IF ERRORLEVEL 1 goto off
+:off
+color 04
+echo          ___________          
+echo   ____  / __/ __/ (_)___  ___ 
+echo  / __ \/ /_/ /_/ / / __ \/ _ \
+echo / /_/ / __/ __/ / / / / /  __/
+echo \____/_/ /_/ /_/_/_/ /_/\___/
+timeout -t 1 -nobreak > NUL
+color 40
+timeout -t 1 -nobreak > NUL
+color 04
+timeout -t 1 -nobreak > NUL
+color 40
+timeout -t 1 -nobreak > NUL
+color 04
+timeout -t 1 -nobreak > NUL
+color 40
+timeout -t 1 -nobreak > NUL
+color 04
+timeout -t 3 -nobreak > NUL
+color 6
+choice /n /c YN /m "Vuoi continuare? (Y/N)" & if errorlevel 2 goto START) || (goto START)
+goto START
